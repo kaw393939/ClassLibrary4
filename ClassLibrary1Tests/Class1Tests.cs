cfd070f6e1640410c9f91912c8556934ce7d7680
Keith Williams <keith@webizly.com>
2019-12-23 15:22:18 +0200

    Add project files.

---
+++ ClassLibrary1Tests/Class1Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
